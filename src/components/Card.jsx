6ea1e7adc26c068cf1f2b41efc93fd8adf5dd216
Devika <dileepdevika3@gmail.com>
2025-08-03 23:48:43 +0530

    fix(): update image size

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,14 +4,12 @@ export default function Card({ moment }) {
   if (!moment) return null
 
   return (
-    <div className="flex-none w-[400px] group cursor-pointer">
+    <div className="flex-none w-[300px] md:w-[400px] group cursor-pointer">
       <div className="relative overflow-hidden">
         <img 
           src={moment.fields.image?.fields.file.url} 
           alt={moment.fields.title}
-          width={400} 
-          height={280}
-          className="w-full h-[280px] object-cover transition-transform duration-500 group-hover:scale-110"
+          className=" w-[300px] md:w-[400px] h-[280px] object-cover transition-transform duration-500 group-hover:scale-110"
         />
       </div>
       
